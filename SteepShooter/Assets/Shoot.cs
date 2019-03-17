43a5aa2cc5f36a21c44c48fd75e538842c228638
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-03-17 11:27:26 +0300

    +Shoot

---
+++ SteepShooter/Assets/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private Camera _camera; //Экземпляр класса тип Camera
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();//Доступ к компонентам
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { //реакция на нажатие кнопки мыши
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0); //середина экрана, половина ширины и высоты
+            Ray ray = _camera.ScreenPointToRay(point);//Создание луча методом.
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {//луч заполняет информацией переменную
+                Debug.Log("Hit " + hit.point);//загружаем координаты точки, в которую луч попал
+            }
+        }
+    }
+}
