f2b22b50e10c09ce6b6bb1102feb7eadc1e1d581
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-04-06 09:33:18 +0300

    + ParticleSystem

---
+++ SteepShooter/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private int _health;
+    // Use this for initialization
+    void Start()
+    {
+        _health = 5;//инициализация переменной
+    }
+    public void Hurt(int damage)
+    {
+        _health -= damage;//уменьшение здоровья игрока
+        Debug.Log("Health: " + _health);
+    }
+
+}
