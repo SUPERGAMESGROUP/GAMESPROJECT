af07a2ca3fc25fc4cc4ca3eee374ae40aa98dbb9
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-04-08 19:29:24 +0300

    + door

---
+++ SteepShooter/Assets/DoorOpenDevice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    public Vector3 dpos;//Смещение, при открывании двери
+    private bool _open;//переменаая да\нет для слежения за открытым состоянием двери
+
+
+    public void Operate(){//открываем или закрываем дверь в зависимости от ее состояния
+        if (_open){
+        Vector3 pos = transform.position - dpos;
+        transform.position = pos;
+        } else{
+            Vector3 pos = transform.position +dpos;
+            transform.position = pos;
+        }
+        _open = !_open;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SteepShooter/Assets/Scripts/BackComet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackComet : MonoBehaviour
+{
+
+    public float speed = 1.0f;//переменная хранит скорость объекта
+    public float maxZ = 16.0f;//точки между которыми будет двигаться объект
+    public float minZ = -16.0f;
+    private int _direction = 1; //хранит в каком направлении движеться объект в данный момент
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        bool bounced = false;//переменная может хранить только false или true
+        if (transform.position.z > maxZ || transform.position.z < minZ)
+        {
+            _direction = -_direction;//меняем на противоположное
+            bounced = true;
+        }
+        if (bounced == true)
+        {//делаем дополнительное движение в кадре, если объект поменял направление
+            transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        }
+
+    }
+}
