92fca82e67ca15493af69c7daf44c2f499aafbc0
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-04-22 08:03:29 +0300

    + 2d Game

---
+++ MemoryCard/Assets/MemoryCard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+
+    public GameObject cardBack; //переменная появиться на панели Inspector
+
+    public SceneController controller;
+
+    private int _id;
+
+    public int id{ get { return _id; } } // добавление функции чтения
+
+    public void SetCard(int id, Sprite image)//метод для передачи указанному объекту новых спрайтов
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)//запускаем код деактивации
+        {
+            cardBack.SetActive(false); //Делаем объект видимым/невидимым
+        }
+    }
+
+    public void Unreveal()
+    { //позволяет вернуть открытую карты на место
+        cardBack.SetActive(true);
+    }
+
+}
---
+++ MemoryCard/Assets/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneController : MonoBehaviour {
+
+    public const int gridRows = 2;//значения, количество ячеей в сетки и их расстояние друг от друга
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    [SerializeField] private MemoryCard originalCard;//ссылка для карты в сцене
+    [SerializeField] private Sprite[] images;//массив ссылок на ресурсы-спрайты
+
+    void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;//Положение первой карты, остальные от этой точки
+
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            { // вложенные циклы, задающие как столбцы, так и строки
+                MemoryCard card; // Ссылка на контейнер для исходной карты или ее копий.
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+                int id = Random.Range(0,images.Length); // Получаем идентификаторы из перемешанного 
+                card.SetCard(id, images[id]);//вызов открытого метода в MemoryCard
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);//в 2d смещаемся только по x  и y
+            }
+        }
+    }
+}
